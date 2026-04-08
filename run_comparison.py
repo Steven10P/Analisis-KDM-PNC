3c9df86e8bff77c53dda917de2192e07240cd44e
Steven10P <bspenad@unal.edu.co>
2026-04-08 22:44:54 +0000

    Reset: Vaciando repositorio por completo en main

--- run_comparison.py
+++
@@ -1,7 +0,0 @@
-"""
-Script principal para ejecutar los experimentos comparativos KDM vs PNC.
-"""
-import argparse
-
-if __name__ == '__main__':
-    print('🚀 Iniciando pipeline de experimentación...')
